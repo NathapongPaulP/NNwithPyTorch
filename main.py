7b133c670b6906c61ddc447cf0594b354a7ca253
NathapongPaulP <nathapongpaulp@gmail.com>
2025-11-28 17:55:20 +0700

    Documented And Savable And Shown Randomly Selected Data

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from torch.utils.data import DataLoader, Dataset
 import matplotlib.pyplot as plt
 import tqdm
 from MNISTS_dataloader import MnistDataloader
+import random
+import math
+from pathlib import Path
 
 
 class SimpleNN(nn.Module):
@@ -57,20 +60,39 @@ def train(model, dataloader, epochs, learning_rate=0.001):
             f"Epoch [{epoch + 1}/{epochs}], Loss: {running_loss / len(dataloader):.4f}"
         )
 
+        checkcpoint_dict = {
+            "epoch": epoch + 1,
+            "model_state_dict": model.state_dict(),
+            "optimizer_state_dict": optimizer.state_dict(),
+            "loss": running_loss / len(dataloader),
+        }
+        torch.save(checkcpoint_dict, f"checkpoint/checkpoint_epoch_{epoch + 1}.pth")
 
-def plot_processed_images(model, dataloader, num_images=5):
+
+def plot_processed_images(model, dataset, num_img_show=25):
+
+    num_shown = 0
+    _ncols = 5
+    _nrows = math.ceil(math.sqrt(num_img_show))
+    random_dataset = [
+        dataset[random.randint(0, len(dataset))] for _ in range(num_img_show)
+    ]
+
     model.eval()
-    images, labels = next(iter(dataloader))
-    outputs = model(images)
-    _, predicted = torch.max(outputs, 1)
+    with torch.no_grad():
+        plt.figure(figsize=(10, 10))
+        for image, label in tqdm.tqdm(random_dataset):
+            if num_shown >= num_img_show:
+                break
+            output = model(image)
+            _, predicted = torch.max(output, 1)
 
-    plt.figure(figsize=(10, 5))
-    for i in range(num_images):
-        plt.subplot(1, num_images, i + 1)
-        plt.imshow(images[i].squeeze(), cmap="gray")
-        plt.title(f"Pred: {predicted[i].item()}\nTrue: {labels[i].item()}")
-        plt.axis("off")
-    plt.show()
+            plt.subplot(_nrows, _ncols, num_shown + 1)
+            plt.imshow(image.squeeze(), cmap="gray")
+            plt.title(f"Pred: {predicted.item()} Actual: {label}")
+            plt.axis("off")
+            num_shown += 1
+        plt.show()
 
 
 def main():
@@ -90,16 +112,22 @@ def main():
 
     (x_train, y_train), (x_test, y_test) = mnist_loader.load_data()
 
-    train_dataset, test_datset = MNISTDataset(x_train, y_train), MNISTDataset(
+    train_dataset, test_dataset = MNISTDataset(x_train, y_train), MNISTDataset(
         x_test, y_test
     )
     train_dataloader = DataLoader(train_dataset, batch_size=64, shuffle=True)
-    test_dataloader = DataLoader(test_datset, batch_size=64, shuffle=False)
+    test_dataloader = DataLoader(test_dataset, batch_size=64, shuffle=False)
 
-    model = SimpleNN()
+    if Path("checkpoint_epoch_5.pth").is_file():
+        checkpoint = torch.load("checkpoint_epoch_5.pth")
+        model = SimpleNN()
+        model.load_state_dict(checkpoint["model_state_dict"])
+        print("Loaded model from checkpoint.")
+    else:
+        model = SimpleNN()
+        train(model, train_dataloader, epochs=5, learning_rate=0.001)
 
-    train(model=model, dataloader=train_dataloader, epochs=5)
-    plot_processed_images(model=model, dataloader=test_dataloader, num_images=5)
+    plot_processed_images(model, test_dataset, num_img_show=25)
 
 
 if __name__ == "__main__":
